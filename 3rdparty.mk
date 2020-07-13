hillu/spyre

commit fa9a644ed784ad41eab5b808f6243bffdff80482

Add rules to save/restore 3rdparty binary artifacts to/from a tarfile

3rdparty.mk
@@ -192,3 +192,27 @@ $(foreach pkg,$(3rdparty_TARGETS),\
 			$(info $(call build_$(pkg)_TEMPLATE,$(arch)))))
 
 .PHONY: 3rdparty-all 3rdparty-clean 3rdparty-distclean 3rdparty-dump-templates
+
+# Save, restore binary artifacts
+3rdparty-artifact-archive := _3rdparty/archive/artifacts-$(shell echo $(foreach pkg,$(3rdparty_TARGETS),$(pkg)=$($(pkg)_VERSION):) | md5sum | awk '{print $$1}').tar.gz
+
+3rdparty-save-artifacts: 3rdparty-all
+	tar -czf $(3rdparty-artifact-archive) \
+		_3rdparty/tgt
+
+3rdparty-restore-artifacts: $(3rdparty-artifact-archive)
+	rm -rf _3rdparty/tgt
+	tar -xzf $< _3rdparty/tgt
+	mkdir -p \
+		$(foreach pkg,$(3rdparty_TARGETS),_3rdparty/src/$(pkg)-$($(pkg)_VERSION)/)\
+		$(foreach pkg,$(3rdparty_TARGETS),\
+			$(foreach arch,$($(pkg)_ARCHS),\
+				_3rdparty/build/$(arch)/$(pkg)-$($(pkg)_VERSION)/))
+	touch \
+		$(foreach pkg,$(3rdparty_TARGETS),_3rdparty/src/$(pkg)-$($(pkg)_VERSION)/.unpack-stamp)\
+		$(foreach pkg,$(3rdparty_TARGETS),\
+			$(foreach arch,$($(pkg)_ARCHS),\
+				_3rdparty/build/$(arch)/$(pkg)-$($(pkg)_VERSION)/.build-stamp))
+	touch 3rdparty-all-stamp
+
+.PHONY: 3rdparty-save-artifacts 3rdparty-restore-artifacts

commit 39444dfc4e086e5bae0bcad8faeaa2395df9fed8

Fix typo in stamp filename
Introduced in fa9a644ed784ad41eab5b808f6243bffdff80482

3rdparty.mk
@@ -213,6 +213,6 @@ $(foreach pkg,$(3rdparty_TARGETS),\
 		$(foreach pkg,$(3rdparty_TARGETS),\
 			$(foreach arch,$($(pkg)_ARCHS),\
 				_3rdparty/build/$(arch)/$(pkg)-$($(pkg)_VERSION)/.build-stamp))
-	touch 3rdparty-all-stamp
+	touch 3rdparty-all.stamp
 
 .PHONY: 3rdparty-save-artifacts 3rdparty-restore-artifacts